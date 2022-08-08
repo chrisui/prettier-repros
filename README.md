a0d47a9e35a049810f84372ed947e1807df88aa1
Chris Pearce <hello@chrispearce.me>
2022-08-09 00:47:16 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prettier Issue Repros
+
+1. https://github.com/prettier/prettier/issues/13225
---
+++ index.ts
@@ -0,0 +1,5 @@
+const ok = <V>(v: V) => v
+const error = <V>(v: V) => v
+
+const okStr = ok<string>
+const errnum = error<number>
